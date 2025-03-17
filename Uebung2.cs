dba719a71c758ea04f0c60cb55ac2028365f15e5
knusperreis23 <knusperreis23@gmail.com>
2025-03-17 14:56:43 +0100

    Uebung2 korrigiert

--- Uebung2.cs
+++ Uebung2.cs
@@ -4,7 +4,7 @@ class Uebung2
 {
     static void Main(string[] args)
     {
-        for (int i = 0; i <= int.MaxValue; i++)
+        for (int i =int.MaxValue-100000; i < int.MaxValue; i++)
         {
             if (i % 2 == 0)
             {
