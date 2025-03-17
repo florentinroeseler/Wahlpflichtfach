f8e025d656b259ef5db3b54aff5e86898b3ebdd5
knusperreis23 <knusperreis23@gmail.com>
2025-03-17 14:37:48 +0100

    Uebung 2 vorläufig
    Vorläufig fertig

---
+++ Uebung2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Uebung2
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= int.MaxValue; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+// Bonusaufgabe: i wird zu -2147483648 wegen eines Overflows. Genauer liegt es
+// daran, dass int intern das Zweierkomplement verwendet.
